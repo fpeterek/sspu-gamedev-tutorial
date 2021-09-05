4052067b592fe3a731af126335320f670f4cf60d
Filip Peterek <filip.peterek@firma.seznam.cz>
2021-09-05 11:07:06 +0200

    Circle hover detection

--- mouse_hover_detection/src/main.cpp
+++ mouse_hover_detection/src/main.cpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <cmath>
+#include <functional>
 
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
@@ -43,6 +44,42 @@ sf::RectangleShape createRect(const sf::Vector2f pos, const float rotation = 0) 
     return rect;
 }
 
+sf::CircleShape createCircle(const sf::Vector2f pos) {
+    sf::CircleShape circle(50);
+    circle.setOrigin(50, 50);
+    circle.setOutlineThickness(3);
+    circle.setPosition(pos);
+    return circle;
+}
+
+std::vector<sf::RectangleShape> createRectangles() {
+    return {
+        createRect({ 300, 370 }, 45),
+        createRect({ 1500, 600 }, 23),
+        createRect({ 680, 850}, 11),
+        createRect({ 940, 500}, 37),
+    };
+}
+
+std::vector<sf::CircleShape> createCircles() {
+    return {
+        createCircle({ 1230, 240 }),
+        createCircle({ 330, 720 }),
+        createCircle({ 1640, 830 }),
+    };
+}
+
+template<typename DestClass, typename OrigClass>
+void addAsReferences(std::vector<std::reference_wrapper<DestClass>> & dest, std::vector<OrigClass> & coll) {
+    for (auto & item : coll) {
+        dest.template emplace_back(item);
+    }
+}
+
+auto createShapes() {
+    return std::make_pair(createRectangles(), createCircles());
+}
+
 float calcDist(const sf::Vector2f p1, const sf::Vector2f p2) {
     const auto dx = p1.x - p2.x;
     const auto dy = p1.y - p2.y;
@@ -72,13 +109,12 @@ float calcAngle(const sf::Vector2f p1, const sf::Vector2f p2) {
     return (p1.y > p2.y) ? acos : (2*M_PI - acos);
 }
 
-bool inside(const sf::RectangleShape & rect, const sf::Vector2i mouse) {
+bool inside(const sf::RectangleShape & rect, const sf::Vector2f mouse) {
 
     auto copy = rect;
-    const sf::Vector2f mf { (float)mouse.x, (float)mouse.y };
-    const auto mouseDist = calcDist(copy.getPosition(), mf);
+    const auto mouseDist = calcDist(copy.getPosition(), mouse);
     const auto rotation = ((360-rect.getRotation()) / (180 / M_PI));
-    const auto mouseAngle = calcAngle(rect.getPosition(), mf);
+    const auto mouseAngle = calcAngle(rect.getPosition(), mouse);
     const auto realAngle = mouseAngle + rotation;
     const sf::Vector2f realMouse {
         copy.getPosition().x + (float)std::cos(realAngle)*mouseDist,
@@ -89,41 +125,63 @@ bool inside(const sf::RectangleShape & rect, const sf::Vector2i mouse) {
     return copy.getGlobalBounds().contains(realMouse);
 }
 
+bool inside(const sf::CircleShape & circle, const sf::Vector2f mouse) {
+    const auto dist = calcDist(circle.getPosition(), mouse);
+    return dist <= circle.getRadius();
+}
+
+void resetColors(std::vector<std::reference_wrapper<sf::Shape>> & shapes) {
+    for (auto & shape : shapes) {
+        shape.get().setFillColor(colors::defaultFill);
+        shape.get().setOutlineColor(colors::defaultOutline);
+    }
+}
+
+template<typename Shape>
+void colorOnHover(std::vector<Shape> & shapes, const sf::Vector2i mouse) {
+    const auto fmouse = sf::Vector2f(mouse.x, mouse.y);
+    for (auto & shape : shapes) {
+        if (inside(shape, fmouse)) {
+            shape.setFillColor(colors::specialFill);
+            shape.setOutlineColor(colors::specialOutline);
+        }
+    }
+}
+
+void handleEvents(sf::RenderWindow & win) {
+    sf::Event ev {};
+    while (win.pollEvent(ev)) {
+        if (closed(ev) or pressed(ev, sf::Keyboard::Q) or pressed(ev, sf::Keyboard::Escape)) {
+            win.close();
+        }
+    }
+}
+
+void redraw(sf::RenderWindow & win, const std::vector<std::reference_wrapper<sf::Shape>> & shapes) {
+    win.clear(sf::Color::White);
+    for (const auto & shape : shapes) {
+        win.draw(shape);
+    }
+    win.display();
+}
+
 int main(int argc, const char * argv[]) {
     auto win = initWindow();
 
-    std::vector<sf::RectangleShape> rectangles {
-        createRect({ 300, 370 }, 45),
-        createRect({ 1500, 600 }, 23),
-        createRect({ 680, 790}, 11),
-    };
+    auto [rectangles, circles] = createShapes();
+    std::vector<std::reference_wrapper<sf::Shape>> shapes;
+    shapes.reserve(rectangles.size() + circles.size());
+    addAsReferences(shapes, rectangles);
+    addAsReferences(shapes, circles);
 
     while (win.isOpen()) {
-
-        for (auto & rect : rectangles) {
-            rect.setFillColor(colors::defaultFill);
-            rect.setOutlineColor(colors::defaultOutline);
-        }
-
-        sf::Event ev {};
-        while (win.pollEvent(ev)) {
-            if (closed(ev) or pressed(ev, sf::Keyboard::Q) or pressed(ev, sf::Keyboard::Escape)) {
-                win.close();
-            }
-        }
+        resetColors(shapes);
+        handleEvents(win);
 
         const auto mouse = sf::Mouse::getPosition(win);
-        for (auto & rect : rectangles) {
-            if (inside(rect, mouse)) {
-                rect.setFillColor(colors::specialFill);
-                rect.setOutlineColor(colors::specialOutline);
-            }
-        }
+        colorOnHover(rectangles, mouse);
+        colorOnHover(circles, mouse);
 
-        win.clear(sf::Color::White);
-        for (const auto & rect : rectangles) {
-            win.draw(rect);
-        }
-        win.display();
+        redraw(win, shapes);
     }
 }
