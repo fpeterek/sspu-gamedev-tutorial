7f32cd17f678fcc9ecb99e74fcbb45ec263446d3
Filip Peterek <filip.peterek@firma.seznam.cz>
2021-09-13 20:39:41 +0200

    Very little progress made on Dino game

--- dino/include/enemy.hpp
+++ dino/include/enemy.hpp
@@ -5,6 +5,8 @@
 #ifndef DINO_ENEMY_HPP
 #define DINO_ENEMY_HPP
 
+#include <random>
+
 #include <SFML/Graphics.hpp>
 
 #include "texture_cycler.hpp"
@@ -12,10 +14,14 @@
 class Enemy {
 
     sf::Sprite sprite;
+    TextureCycler textureCycler;
+    sf::Vector2f hitboxSize;
+    Enemy(TextureCycler cycler, float scale, sf::Vector2f position, sf::Vector2f hitboxSize);
 
 public:
 
-    Enemy(TextureCycler cycler, float scale, sf::Vector2f position);
+    sf::FloatRect hitbox() const;
+    void update(float dt);
     void move(sf::Vector2f delta);
 
     class Factory {
@@ -25,6 +31,10 @@ public:
         const float scale;
         const std::pair<uint, uint> yRange;
         const uint windowWidth;
+        std::default_random_engine rand;
+
+        Enemy createBird();
+        Enemy createCactus();
 
     public:
         Factory(TextureCycler cactus, TextureCycler bird, float scale, decltype(yRange) yRange, uint winWidth);
--- dino/src/enemy.cpp
+++ dino/src/enemy.cpp
@@ -3,12 +3,52 @@
 //
 
 #include <enemy.hpp>
+#include <utility>
 
 Enemy::Factory::Factory(
     TextureCycler cactus, TextureCycler bird, const float scale, const decltype(yRange) yRange, const uint winWidth) :
-    cactus(std::move(cactus)), bird(std::move(bird)), scale(scale), yRange(yRange), windowWidth(winWidth) {
+    cactus(std::move(cactus)), bird(std::move(bird)), scale(scale), yRange(yRange), windowWidth(winWidth),
+    rand(time(nullptr)) { }
 
-
-
+Enemy Enemy::Factory::create() {
+    if (std::uniform_int_distribution(1, 3)(rand) == 3) {
+        return createBird();
+    }
+    return createCactus();
 }
 
+// TODO: Fix position
+Enemy Enemy::Factory::createBird() {
+    return Enemy(bird, scale, { (float)windowWidth, (float)yRange.first }, { 14.f, 6.f });
+}
+
+Enemy Enemy::Factory::createCactus() {
+    return Enemy(cactus, scale, { (float)windowWidth, (float)yRange.second }, { 10.f, 14.f });
+}
+
+sf::FloatRect Enemy::hitbox() const {
+    return {
+        sprite.getPosition().x + (sprite.getLocalBounds().width - hitboxSize.x) / 2,
+        sprite.getPosition().y + (sprite.getLocalBounds().height - hitboxSize.y) / 2,
+        hitboxSize.x,
+        hitboxSize.y
+    };
+}
+
+void Enemy::update(const float dt) {
+    textureCycler.update(dt);
+    sprite.setTexture(textureCycler.current());
+}
+
+void Enemy::move(const sf::Vector2f delta) {
+    sprite.move(delta);
+}
+
+Enemy::Enemy(TextureCycler cycler, float scale, sf::Vector2f position, sf::Vector2f hitboxSize) :
+    textureCycler(std::move(cycler)), hitboxSize(hitboxSize) {
+
+    sprite.setTexture(textureCycler.current());
+    sprite.setScale(scale, scale);
+    sprite.setPosition(position);
+
+}
